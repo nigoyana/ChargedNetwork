ec1691b0a6b709945bf73ace7be1e4617625af49
Artem Nigoian <nigoyana@naf-cms13.desy.de>
2019-07-18 15:13:04 +0200

    separate histograms for each H mass

--- bin/mass.py
+++ bin/mass.py
@@ -13,72 +13,120 @@ import tensorflow as tf
 def parser():
     parser = argparse.ArgumentParser(description='Program for predict charged Higgs mass', formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('--train', action = "store_true", help="Train model on data")
-
+    parser.add_argument('--tune', action = "store_true", help="Tune the hyperparameters")
     return parser.parse_args()
 
-def trainModel():
-    data = []
-    result = []
 
-    nEpoch = 1000
+def tryModel(model, dataTrain, resultTrain, dataTest, resultTest, nEpoch, bkgTest, dictMassInd):
+	callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10)
+	model.compile(optimizer=tf.keras.optimizers.RMSprop(0.001), loss='mean_squared_error', metrics=['mean_squared_error'])
+	training = model.fit(dataTrain, resultTrain, epochs=nEpoch, batch_size=25, callbacks=[callback], validation_split=0.1, verbose=2)
+	model.summary()
+	os.makedirs("models/" + model.title, exist_ok=True)
+	path_to_save = "models/" + model.title + "/" + model.title
+	model.save_weights(path_to_save, save_format='tf')
+	##Check mass distribution on test data
+	##masses = xrange(200, 650, 50)
+	bkgPrediction = model.predict(bkgTest).flatten()
+	for mass in dictMassInd:
+		start = dictMassInd[mass][0]
+		end = dictMassInd[mass][1]
+		signalPrediction = model.predict(dataTest[start:end]).flatten()
+		model.plotTraining(training, signalPrediction, bkgPrediction, resultTest[start:end], mass)
 
-    dataTest, resultTest = [], []
-	
-    data_files = ["data/massTraining/e4j/L4B_200_100.csv", 
-		  "data/massTraining/e4j/L4B_400_100.csv", 
-		  "data/massTraining/e4j/L4B_600_100.csv",
-		  "data/massTraining/mu4j/L4B_200_100.csv", 
-		  "data/massTraining/mu4j/L4B_400_100.csv", 
-		  "data/massTraining/mu4j/L4B_600_100.csv"]
 
+def trainModel(tuneFlag):
+	data = []
+	result = []
+
+	nEpoch = 500
+
+	dataTest, resultTest = [], []
+
+	data_files = [
+			"data/massTraining/mu4j/L4B_200_100.csv",
+			"data/massTraining/mu4j/L4B_250_100.csv",
+			"data/massTraining/mu4j/L4B_300_100.csv",
+			"data/massTraining/mu4j/L4B_350_100.csv",
+			"data/massTraining/mu4j/L4B_400_100.csv",
+			"data/massTraining/mu4j/L4B_450_100.csv",
+			"data/massTraining/mu4j/L4B_500_100.csv",
+			"data/massTraining/mu4j/L4B_550_100.csv",
+			"data/massTraining/mu4j/L4B_600_100.csv"]
+
+	dictFiles = dict()
+
+	for file_name in data_files:
+		mass = int(file_name[-11:-8])
+		if mass in dictFiles:
+			dictFiles[mass].append(file_name)
+		else:
+			dictFiles[mass] = [file_name]
+
+	print("dictFiles: ", dictFiles)
     ##Read csv data to numpy
-    for file_name in data_files:
-        dataFrame = pd.read_csv(file_name, sep=",")
-        data = dataFrame.to_numpy()
-        trainFrac = int(0.9 * data.shape[0])
-        if (data_files.index(file_name) == 0):
-            dataTest, resultTest = data[trainFrac::, :-12],  data[trainFrac::, [-1]]
-            dataTrain, resultTrain = data[:trainFrac:, :-12], data[:trainFrac:, [-1]]
-        else:
-            dataTest = np.append(dataTest, data[trainFrac::, :-12], axis = 0)
-            resultTest = np.append(resultTest, data[trainFrac::, [-1]], axis = 0)	
-            dataTrain = np.append(dataTrain, data[:trainFrac:, :-12], axis = 0)
-            resultTrain = np.append(resultTrain, data[:trainFrac:, [-1]], axis = 0)
+	dictMassInd = dict()
+	dataTest = []
+	for mass in dictFiles:
+		start_index = len(dataTest)
+		for file_name in dictFiles[mass]:
+			dataFrame = pd.read_csv(file_name, sep=",")
+			data = dataFrame.to_numpy()
+			trainFrac = int(0.9 * data.shape[0])
+			if (data_files.index(file_name) == 0):
+				#dataTest = data[trainFrac::, :-12]
+				#resultTest = [int(file_name[-11:-8])] * data[trainFrac::, [-1]].shape[0]
+				#dataTrain = data[:trainFrac:, :-12]
+				#resultTrain = [int(file_name[-11:-8])] * data[:trainFrac:, [-1]].shape[0]
+				dataTest, resultTest = data[trainFrac::, :-12], data[trainFrac::, [-1]]
+				dataTrain, resultTrain = data[:trainFrac:, :-12], data[:trainFrac:, [-1]]
+			else:
+				dataTest = np.append(dataTest, data[trainFrac::, :-12], axis = 0)
+				resultTest = np.append(resultTest, data[trainFrac::, [-1]], axis = 0)
+				#resultTest = np.append(resultTest, [int(file_name[-11:-8])] * data[trainFrac::, [-1]].shape[0])
+				dataTrain = np.append(dataTrain, data[:trainFrac:, :-12], axis = 0)
+				resultTrain = np.append(resultTrain, data[:trainFrac:, [-1]], axis = 0)
+				#resultTrain = np.append(resultTrain, [int(file_name[-11:-8])] * data[:trainFrac:, [-1]].shape[0])
+		end_index = len(dataTest)
+		dictMassInd[mass] = (start_index, end_index)
+		print(dictMassInd[mass])
 
-    bkgFrame = pd.read_csv("data/massTraining/e4j/TT+j-1L.csv", sep=",")
+	print(dictMassInd)
+	
+	bkgFrame = pd.read_csv("data/massTraining/e4j/TT+j-1L.csv", sep=",")
 
-    bkgTest = bkgFrame.to_numpy()[:40000:, :-12],
+	bkgTest = bkgFrame.to_numpy()[:40000:, :-12],
 
     ##Train model
 
-    max_num_of_layers = 5
-    max_num_of_nodes = 101
-    step_nodes = 50
-    activation_functions = ["relu", "elu", "linear", "selu", "softplus"]
+	max_num_of_layers = 5
+	max_num_of_nodes = 101
+	step_nodes = 50
+	activation_functions = ["relu", "elu", "linear", "selu", "softplus"]
 
-    for number_of_layers in range(2, max_num_of_layers):
-        for activation in activation_functions:
-            for number_of_nodes in range(50, max_num_of_nodes, step_nodes):
-                model = MassModel(dataTrain.shape[-1], 3, number_of_layers, number_of_nodes, activation, 0.4)
-                callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10)
-                model.compile(optimizer=tf.keras.optimizers.RMSprop(0.001), loss='mean_squared_error', metrics=['mean_squared_error'])
-                training = model.fit(dataTrain, resultTrain, epochs=nEpoch, batch_size=25, callbacks=[callback], validation_split=0.1, verbose=2)
-                model.summary()
-                path_to_save = "models/" + model.title + "/" + model.title
-                os.makedirs("models/" + model.title, exist_ok=True)
-                model.save_weights(path_to_save, save_format='tf')
-                ##Check mass distribution on test data
-                signalPrediction = model.predict(dataTest).flatten()
-                bkgPrediction = model.predict(bkgTest).flatten()
-                model.plotTraining(training, signalPrediction, bkgPrediction, resultTest)
+	if (tuneFlag):
+		for number_of_layers in range(2, max_num_of_layers):
+			for activation in activation_functions:
+				for number_of_nodes in range(75, max_num_of_nodes, step_nodes):
+					model = MassModel(dataTrain.shape[-1], 3, number_of_layers, number_of_nodes, activation, 0.4)
+					print("Training model " + model.title)
+					tryModel(model, dataTrain, resultTrain, dataTest, resultTest, nEpoch, bkgTest, dictMassInd)
+	else:
+		model = MassModel(dataTrain.shape[-1], 3, 3, 150, "elu", 0.4)
+		print("Training model " + model.title)
+		tryModel(model, dataTrain, resultTrain, dataTest, resultTest, nEpoch, bkgTest, dictMassInd)
+
 
 def main():
     ##Parser arguments
-    args = parser()
+	args = parser()
 
     ##Train model
-    if args.train:         
-          trainModel()  
+	if args.train:
+		tuneFlag = False
+		if args.tune:
+			tuneFlag = True
+		trainModel(tuneFlag)
 
 if __name__ == "__main__":
     main()
--- python/massmodel.py
+++ python/massmodel.py
@@ -2,45 +2,49 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 class MassModel(tf.keras.Model):
-    def __init__(self, inputShape, nClass, nLayer=3, nNodes=100, activation="relu", dropout=0.3):
-        self.nLayers = nLayer
-        self.nClass = nClass
-        self.title = "l" + str(nLayer) + "_n" + str(nNodes) + "_" + str(activation) + "_mu_and_e"
+	def __init__(self, inputShape, nClass, nLayer=3, nNodes=100, activation="relu", dropout=0.3):
+		self.nLayers = nLayer
+		self.nClass = nClass
+		self.title = "l" + str(nLayer) + "_n" + str(nNodes) + "_" + str(activation) + "_mu" #"_mu_and_e"
 
-        inputLayer = tf.keras.layers.Input(shape=(inputShape))
-        x = tf.keras.layers.Dense(nNodes, activation=activation)(inputLayer)
+		inputLayer = tf.keras.layers.Input(shape=(inputShape))
+		x = tf.keras.layers.Dense(nNodes, activation=activation)(inputLayer)
 
-        for n in range(nLayer-1):
-            x = tf.keras.layers.Dense(nNodes, activation=activation)(x)
+		for n in range(nLayer-1):
+			x = tf.keras.layers.Dense(nNodes, activation=activation)(x)
 
-        outputLayer = tf.keras.layers.Dense(1)(x)
+		outputLayer = tf.keras.layers.Dense(1)(x)
 
-        tf.keras.Model.__init__(self, inputs=inputLayer, outputs=outputLayer, name="MassModel")
+		tf.keras.Model.__init__(self, inputs=inputLayer, outputs=outputLayer, name="MassModel")
 
-    def plotTraining(self, training, signal, background, true):
-        fig, ax = plt.subplots()	
-        x_length = len(training.history['mean_squared_error'])
-        ax.plot(range(x_length), training.history['mean_squared_error'], label = "Training set")
-        ax.plot(range(x_length), training.history['val_mean_squared_error'], label = "Validation set")
-        ax.set_ylabel('mean squared error')
-        ax.set_xlabel('Number of epochs')
-        ax.set_title("Mean squared error of training/validation sample")
-        ax.legend(loc="center left", bbox_to_anchor=(1.0, 0.5))
+#	def __call__(self):
+#		tryModel()
 
-        fig.subplots_adjust(wspace=0.3, hspace=0.7)
-        training_name = "models/" + self.title + "/" + "training_" + self.title + ".pdf"     
-        fig.savefig(training_name, bbox_inches="tight")
+	def plotTraining(self, training, signal, background, true, mass):
+		fig, ax = plt.subplots()
+		x_length = len(training.history['mean_squared_error'])
+		ax.plot(range(x_length), training.history['mean_squared_error'], label = "Training set")
+		ax.plot(range(x_length), training.history['val_mean_squared_error'], label = "Validation set")
+		ax.set_ylabel('mean squared error')
+		ax.set_xlabel('Number of epochs')
+		ax.set_title("Mean squared error of training/validation sample")
+		ax.legend(loc="center left", bbox_to_anchor=(1.0, 0.5))
 
-        fig, ax = plt.subplots()
-        ax.hist(true, bins=15, label = "True", histtype="step",normed=True)
-        ax.hist(signal, bins=15, label = "Prediction (signal)", histtype="step", normed=True)
-        ax.hist(background, bins=15, label = "Prediction (background)", histtype="step", normed=True)
-        ax.set_ylabel("Number of events")
-        ax.set_xlabel(r'm(H$^\pm$)')
-        ax.set_title(r"Mass reconstruction of H$^\pm$")
-        ax.legend(loc="center left", bbox_to_anchor=(1.0, 0.5))
+		fig.subplots_adjust(wspace=0.3, hspace=0.7)
+		training_name = "models/" + self.title + "/" + "training_" + self.title + ".pdf"
+		fig.savefig(training_name, bbox_inches="tight")
 
-        fig.subplots_adjust(wspace=0.3, hspace=0.7)
-        mass_name = "models/" + self.title + "/" + "mass_" + self.title + ".pdf"
-        fig.savefig(mass_name, bbox_inches="tight")
+		fig, ax = plt.subplots()
+		ax.hist(true, bins=80, label = "True", histtype="step",normed=True)
+		ax.hist(signal, bins=80, label = "Prediction (signal)", histtype="step", normed=True)
+		ax.hist(background, bins=80, label = "Prediction (background)", histtype="step", normed=True)
+		ax.set_ylabel("Number of events")
+		ax.set_xlabel(r'm(H$^\pm$)')
+		ax.set_xlim(0, 800)
+		ax.set_title(r"Mass reconstruction of H$^\pm$")
+		ax.legend(loc="center left", bbox_to_anchor=(1.0, 0.5))
+
+		fig.subplots_adjust(wspace=0.3, hspace=0.7)
+		mass_name = "models/" + self.title + "/" + "mass" + str(mass) + "_" + self.title + ".pdf"
+		fig.savefig(mass_name, bbox_inches="tight")
 
