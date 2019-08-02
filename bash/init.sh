5af25d454d86a0292aa9beb42f73c5486f683e62
DaveBrun94 <david.brunner@desy.de>
2019-08-02 11:27:07 +0200

    Fixed dir because of new directory hierachy

--- bash/init.sh
+++ bash/init.sh
@@ -3,4 +3,4 @@ export NETPATH=$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")
 export PYTHONPATH=$NETPATH/anaconda3/lib/python3.7/site-packages
 PYTHONPATH=$PYTHONPATH:$NETPATH/python
 
-PATH=$PATH:$NETPATH/bin
+PATH=$PATH:$NETPATH/Network/scripts
