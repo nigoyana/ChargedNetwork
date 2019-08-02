d0e81b704c4c68655606aaaace75f80888184e11
DaveBrun94 <david.brunner@desy.de>
2019-08-02 11:32:39 +0200

    Added more stuff

--- bash/init.sh
+++ bash/init.sh
@@ -1,6 +1,6 @@
 export NETPATH=$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")
 
 export PYTHONPATH=$NETPATH/anaconda3/lib/python3.7/site-packages
-PYTHONPATH=$PYTHONPATH:$NETPATH/python
+PYTHONPATH=$PYTHONPATH:$NETPATH/Network/python
 
 PATH=$PATH:$NETPATH/Network/scripts
